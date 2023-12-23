fec05d81513016c9e202f757888e88ab63d487c8
Willer <willerbarros137@gmail.com>
2023-12-23 18:50:23 -0300

    correacao

--- dividindo_dt.py
+++ dividindo_dt.py
@@ -4,6 +4,15 @@ url = r'c:\Users\Willer\Desktop\dividindo_dados.csv'
 
 df = pd.read_csv(url, sep=',', encoding='windows-1252')
 
-print(df.shape)
+df_parte_01 = df.iloc[:50, :]
+df_parte_02 = df.iloc[50:, :]
+
+
+try:
+    df_parte_01.to_csv('divisao_01.csv', index=False)
+    print("Download realizado com sucesso")
+except:
+    print("Falha no Download")
+
 
 
